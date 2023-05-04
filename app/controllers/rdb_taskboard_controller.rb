5577658f6f3177f4cec64e1eb65a22952d9882ac
Marcel Rufino de Carvalho <marcelrufino@gmail.com>
2023-05-04 17:14:24 -0300

    Mensagem de erro

--- app/controllers/rdb_taskboard_controller.rb
+++ app/controllers/rdb_taskboard_controller.rb
@@ -63,7 +63,7 @@ class RdbTaskboardController < RdbDashboardController
           },
         )
       else
-        flash_custom_error @issue.errors.messages[0], issue: @issue.subject, source: @issue.status.name, target: @status.name
+        flash_custom_error @issue.errors.messages[0], issue: @issue.subject
         raise ActiveRecord::Rollback
       end
     end
