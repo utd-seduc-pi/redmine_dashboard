abee0b1badb94d8c984f16eb7171fedf49143765
Marcel Rufino de Carvalho <marcelrufino@gmail.com>
2023-05-04 16:20:55 -0300

    Categoria com Projeto

--- app/models/rdb_category_filter.rb
+++ app/models/rdb_category_filter.rb
@@ -46,9 +46,7 @@ class RdbCategoryFilter < RdbFilter
     return I18n.t(:rdb_filter_category_multiple) if values.count > 1
 
     category = board.issue_categories.find_by_id(value)
-    new_title = "#{category.project.try(:name)} - #{category.try(:name)}"
-
-    return new_title
+    "#{category.project.try(:name)} - #{category.try(:name)}"
   end
 
   def enabled?(id)
